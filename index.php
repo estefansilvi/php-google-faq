6c3d77790b1635fd14ac69ce6259ff8676c11473
Stefania <estefansilvi@gmail.com>
2021-03-01 22:16:41 +0100

    correzione elenco di elenco

--- index.php
+++ index.php
@@ -32,13 +32,11 @@
       "answer"=>"Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
       </br>
       <ol>
-        <li> La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-          </br>
+        <li> La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
           <ol class='alpha'>
-            <li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li></br>
-            <li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></br>
+            <li> Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+            <li> Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
           </ol>
-        </li></br>
         <li> La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
       </ol>
       </br>
@@ -98,11 +96,11 @@
             echo $articolo['question'];
             ?>
           </h1>
-          <p class="answer">
+          <div class="answer">
             <?php
             echo $articolo['answer'];
             ?>
-          </p>
+          </div>
 
           <?php
         }
